4fcbf8f7cc560cb86d03ed4681b02344f1879a2f
NicoCaro <ncaro@dim.uchile.cl>
2018-10-24 14:34:01 -0300

    v1.2

--- figuras/tikz/cleaner.py
+++ figuras/tikz/cleaner.py
@@ -2,24 +2,36 @@
 Script de limpieza (Python 3). Borra las extensiones producidas
 al generar las imagenes y mueve los .pdf's resultantes a la carpeta
 '../pdf/'
+
+Se Agrega Try + catch para compatiblidad con Windows. En linux
+basta con os.rename, en Windows es neceesario eliminar el pdf
+previo.
 '''
 
 import os
 from path import Path
 
-dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+dir_path     = os.path.dirname(os.path.realpath(__file__))
+dir_path , _ = os.path.split(dir_path)
 
-files = dir_path.walkfiles('*')
+tikz_folder = Path(dir_path + '\\tikz\\')
+pdf_folder  = Path(dir_path + '\\pdf\\')
+
+files = tikz_folder.walkfiles('*')
 
 ext = ['.aux','latexmk','.fls','.log','gz']
 ext = ['*' + e for e in ext]
 
 for e in ext:
-    files = dir_path.walkfiles(e)
+    files = tikz_folder.walkfiles(e)
     for file in files:
         file.remove()
 
-pdfs = dir_path.walkfiles('*.pdf')
-
+pdfs = tikz_folder.walkfiles('*.pdf')
 for file in pdfs:
-    os.rename(file,'../pdf')
+    _ , name = os.path.split(file)
+    try:
+        os.rename(file, pdf_folder+name)
+    except:
+        os.remove(pdf_folder + name)
+        os.rename(file, pdf_folder + name)
